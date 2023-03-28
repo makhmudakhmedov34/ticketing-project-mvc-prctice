2a6576a8dad875a264f7a99f70f2d2cba1339e1a
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-03-27 22:19:24 -0400

    Create Services

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -5,13 +5,6 @@ import com.cydeo.dto.UserDTO;
 
 import java.util.List;
 
-public interface RoleService {
-
-    RoleDTO save(RoleDTO role);
-    RoleDTO findByID(Long id);
-    List<RoleDTO> findAll();
-    void delete(RoleDTO role);
-    void deleteById(Long id);
-
+public interface RoleService extends CrudService<RoleDTO,String> {
 
 }
