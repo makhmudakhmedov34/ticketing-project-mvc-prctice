d02e739c3147f608d58c2b8904656b14895546ee
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-03-28 23:56:35 -0400

    Create Services

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.RoleDTO;
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +12,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+   private RoleService roleService;
+   private UserService userService;
+
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
     @GetMapping("/create")
     public String createUser(Model model){
 
         model.addAttribute("user",new UserDTO());
+        model.addAttribute("roles",roleService.findAll());
 
 
         return "user/create";
--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -5,6 +5,6 @@ import com.cydeo.dto.UserDTO;
 
 import java.util.List;
 
-public interface RoleService extends CrudService<RoleDTO,String> {
+public interface RoleService extends CrudService<RoleDTO,Long> {
 
 }
